641a8bf61cd343f2acbebce01c52f6fc9368183e
Aminata <aminata.kane.10@gmail.com>
2018-10-10 11:18:59 +0200

    depot initial

---
+++ src/main/java/fr/dauphine/ja/kaneAminata/shapes/Point.java
@@ -0,0 +1,14 @@
+package fr.dauphine.ja.kaneAminata.shapes;
+
+public class Point {
+	private int x;
+	private int y ; 
+	
+	 public static void main( String[] args ) {
+		Point p = new Point();
+		System.out.println(p.x+" "+p.y);
+	
+	}
+
+
+}
---
+++ src/main/java/fr/dauphine/ja/kaneAminata/shapes/testPoint.java
@@ -0,0 +1,11 @@
+package fr.dauphine.ja.kaneAminata.shapes;
+
+public class testPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p = new Point();
+		System.out.println(p.x+" "+p.y);
+	}
+
+}
